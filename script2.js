272342aefddc4325a33152739258d43c14c295bf
ITStepEric <eric.arechiga@itstepuniv.onmicrosoft.com>
2022-10-21 21:42:59 -0700

    Agregar .gitignore y primeros archivos del proyecto

---
+++ script2.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('#form');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+
+form.addEventListener('submit', sumarInputValues);
+
+// function sumarInputValues(event) {
+//     //console.log({ event });
+//     event.preventDefault();
+//     const sumaInputs = input1.value + input2.value;
+//     pResult.innerText = "Resultado: " + sumaInputs;
+// }
+
+btn.addEventListener('submit', sumarInputValues);
+
+function sumarInputValues(event) {
+    //console.log({ event });
+    event.preventDefault();
+    const sumaInputs = input1.value + input2.value;
+    pResult.innerText = "Resultado: " + sumaInputs;
+}
+
